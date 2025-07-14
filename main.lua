8cc68589506add1721de3a893b07f01b1bdbe85a
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-14 12:46:34 +0400

    Added player and player movement

--- main.lua
+++ main.lua
@@ -1,13 +1,32 @@
 _G.love = require("love")
 
 function love.load()
-
+    player = {
+        x = 100,
+        y = 100,
+        radius = 30,
+        speed = 5,
+    }
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed
+    end
 
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed
+    end
+
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
+    end
+
+    if love.keyboard.isDown("s") then
+        player.y = player.y + player.speed
+    end
 end
 
 function love.draw()
-
+    love.graphics.circle("fill", player.x, player.y, player.radius)
 end
